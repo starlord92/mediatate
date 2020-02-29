46fc744497ea2dd83443f0a2b77071fceb97a81b
Mark Phamgit confid --global user.email mark.pham.developer@gmail.comgit config --global user.name Mark <mark.pham.developer@gmail.com>
2020-02-28 18:54:01 -0800

    user click a to start a timer, which rings a bell once when it ends

---
+++ timer_interface/bell_script.js
@@ -0,0 +1,7 @@
+var bell = document.getElementById("zenbellsound");
+
+function ringBell() {
+	bell.play();
+}
+
+ringBell();
--- timer_interface/timer_script.js
+++ timer_interface/timer_script.js
@@ -1,16 +1,57 @@
-
+//https://www.w3schools.com/howto/howto_js_countdown.asp
 	
 	var five = 6000;
 
-	// Update the count down every 1 second
-	var x = setInterval(function() {
-	five = five - 1000;
-	var seconds = five/1000;
-	// Output the result
-	document.getElementById("timer").innerHTML = seconds + "";    
+	var interval;
+
+	// update the count down every 1 second
+
+	var x = function updateTimer() {
+
+		five = five - 1000;
+		var seconds = five/1000;
+	
+		document.getElementById("timer").innerHTML = seconds + "";    
 		if (five < 0) {
-			clearInterval(x);
-			document.getElementById("timer").innerHTML = "time is up";
+			clearInterval(interval);
+			document.getElementById("timer").innerHTML = "approach each task without judgement";
+			var bell_sound = document.getElementById("zenbellsound");
+			bell_sound.play();
+			
 		}
-	}, 1000);
+	}
+
+	function startTimer(){
+		interval = setInterval(x, 1000);	
+	}
+
+
+
+	// var five = 6000;
+
+	// // update the count down every 1 second
+
+	// var x = setInterval(function() {
+	// 	five = five - 1000;
+	// 	var seconds = five/1000;
+	// 	// Output the result
+	// 	document.getElementById("timer").innerHTML = seconds + "";    
+	// 	if (five < 0) {
+
+	// 		clearInterval(x);
+
+	// 		document.getElementById("timer").innerHTML = "";
+	
+			
+
+	// 		// var audio = new Audio('sound_effects/bell.mp3');
+	// 		// audio.play();
+
+	// 		var bell_sound = document.getElementById("zenbellsound");
+	// 		bell_sound.play();		
+	// 	}
+	// }, 1000);
+
+
+
 		
