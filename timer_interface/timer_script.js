00dd6b423e09c565ba0c986da329cede9fda3207
Mark Phamgit confid --global user.email mark.pham.developer@gmail.comgit config --global user.name Mark <mark.pham.developer@gmail.com>
2020-02-29 09:30:08 -0800

    comply with chrome security directive by move onclick code to JS file

--- timer_interface/timer_script.js
+++ timer_interface/timer_script.js
@@ -1,56 +1,36 @@
 //https://www.w3schools.com/howto/howto_js_countdown.asp
-	
-	var five = 6000;
 
-	var interval;
 
-	// update the count down every 1 second
+var five = 60000;
 
-	var x = function updateTimer() {
+var interval;
 
-		five = five - 1000;
-		var seconds = five/1000;
-	
-		document.getElementById("timer").innerHTML = seconds + "";    
-		if (five < 0) {
-			clearInterval(interval);
-			document.getElementById("timer").innerHTML = "approach each task without judgement";
-			var bell_sound = document.getElementById("zenbellsound");
-			bell_sound.play();
+// update the count down every 1 second
+
+var x = function updateTimer() {
+	five = five - 1000;
+	var seconds = five/1000;
+	document.getElementById("timer").innerHTML = seconds + "";    
+	if (five < 0) {
+		clearInterval(interval);
+		document.getElementById("timer").innerHTML = "approach each task without judgement";
+		var bell_sound = document.getElementById("zenbellsound");
+		bell_sound.play();
 			
-		}
 	}
+}
 
-	function startTimer(){
-		interval = setInterval(x, 1000);	
-	}
-
+document.getElementById("timer").addEventListener("click", startTimer);
 
 
-	// var five = 6000;
+function startTimer() {
+	var bell_sound = document.getElementById("zenbellsound");
+	bell_sound.play();
 
-	// // update the count down every 1 second
+	interval = setInterval(x, 1000);	
+}
 
-	// var x = setInterval(function() {
-	// 	five = five - 1000;
-	// 	var seconds = five/1000;
-	// 	// Output the result
-	// 	document.getElementById("timer").innerHTML = seconds + "";    
-	// 	if (five < 0) {
 
-	// 		clearInterval(x);
-
-	// 		document.getElementById("timer").innerHTML = "";
-	
-			
-
-	// 		// var audio = new Audio('sound_effects/bell.mp3');
-	// 		// audio.play();
-
-	// 		var bell_sound = document.getElementById("zenbellsound");
-	// 		bell_sound.play();		
-	// 	}
-	// }, 1000);
 
 
 
