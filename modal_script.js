0ccdaea9929bdb8adb01db3a8f6509a596bf334d
Mark Phamgit confid --global user.email mark.pham.developer@gmail.comgit config --global user.name Mark <mark.pham.developer@gmail.com>
2020-03-27 21:00:45 -0700

    fixing new modal_script

--- modal_script.js
+++ modal_script.js
@@ -1,12 +1,21 @@
-window.onload = function () {
 
-	// Get the modal
+//inject modal.html, a modal ovelaying the distracting sites.
+$(document).ready(function() {
+ 
+	$.get(chrome.extension.getURL('/modal.html'), function(data) {
+	    $(data).appendTo('body');
+	});
+ 
+});
+
+
+// Get the modal
 	var modal = document.getElementById("myModal");
 	// Get the <span> element that closes the modal
 	// var span = document.getElementsByClassName("close")[0];
 
 
-var time = 30000;
+var time = 3000;
 
 var interval;
 
@@ -39,22 +48,6 @@ var x = function updateTimer() {
 };
 
 
-
-//user wants to access the distracting site	
-// var proceed_to_site = document.getElementById("proceed");
-// proceed_to_site.addEventListener("click", 
-// 	function() {
-// 		modal.style.display = "none";
-// 		window.location = "https://www.google.com";
-// 	}
-// );
-
-
-
-
-
-
-
 function startTimer() {
 	var bell_sound = document.getElementById("zenbellsound");
 	// bell_sound.play();
@@ -69,40 +62,3 @@ window.onload = function () {
 
 
 console.log('timer script running ' + time);
-
-	//var modal = document.getElementById("myModal");
-
-	// Get the button that opens the modal
-	var btn = document.getElementById("myBtn");
-
-	// Get the <span> element that closes the modal
-	//var span = document.getElementsByClassName("close")[0];
-
-
-	// // When the user clicks on <span> (x), close the modal
-	// span.onclick = function() {
-	//  modal.style.display = "none";
-	// }
-
-
-	var close_btn = document.getElementById("close5019550752");
-	close_btn.addEventListener("click", 
-		function() {
-			modal.style.display = "none";
-		}
-	);
-
-	//When the user clicks anywhere outside of the modal, close it
-	window.onclick = function(event) {
-	  if (event.target == modal) {
-	    modal.style.display = "none";
-	    
-	  }
-	}
-
-
-
-
-
-
-};
