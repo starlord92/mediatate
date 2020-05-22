45a97e3f1058c4d0772acbd82f413944aa5b63a0
Mark Pham <markpham@Marks-iMac.local>
2020-05-21 20:47:44 -0700

    separate the <body> grid of settins page from other pages

--- user_profile_settings.js
+++ user_profile_settings.js
@@ -20,18 +20,23 @@ function hide_headspace_page() {
 
 
 function hide_settings_page () {
+		$('html').removeClass();
+		$('body').removeClass('settings_page_body');
+		
 		$(".user_profile_id_9809403065").hide();
 		$(".user_profile_section_show_9809403065").hide();
 		$(".user_profile_menu_show_9809403065").hide();
 		$(".user_profile_section_and_menu_grid_divider_show_9809403065").hide()
 
-		$('html').removeClass();
+
 };
 
 function hide_meditations_page () {
-		$('.recommended_meditation_recording_9809403065').hide();
+		;
 		$('html').removeClass();
 		$('body').removeClass('meditations_page_body');
+
+		$('.recommended_meditation_recording_9809403065').hide()
 		$('.recording_lists_9809403065').hide();
 		//we should NOT need this bevause by default the below class is hidden
 		// $('individual_meditation_recording_player_9809403065').hide();
@@ -54,6 +59,9 @@ function show_settings_page() {
 	hide_meditations_page();
 
 	$('html').addClass('settings_page_background');
+	$('body').addClass('settings_page_body');
+
+
 	$(".user_profile_id_9809403065").show();
 	$(".user_profile_section_show_9809403065").show();
 	$(".user_profile_menu_show_9809403065").show();
