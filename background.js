5fe620335abdcafc3aea1d0b6d4f10c9ba9ed5f7
Mark Pham <markpham@Marks-iMac.local>
2020-05-28 14:49:56 -0700

    adding darkening effects

--- background.js
+++ background.js
@@ -128,11 +128,10 @@ function checkScheduledMeditationTime() {
 				//onsole.log('start work time < end work time');
 
 				//25 - 20 seconds before: send content script a message to darken the current active window (0% -> 30% )
-				if(curr_time.getMinutes() == 59 && curr_time.getSeconds() == 30){			
+				if(curr_time.getMinutes() == 36 && curr_time.getSeconds() == 30){			
 					chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-					  chrome.tabs.sendMessage(tabs[0].id, {message: "darken the screen"}, function(response) {
-					    console.log(response.message);
-					  });
+					  chrome.tabs.sendMessage(tabs[0].id, {message: "darken the screen"}, function(response) {;}
+					  );
 					});
 				}
 
@@ -370,227 +369,227 @@ chrome.storage.onChanged.addListener(function () {
 
 
 
-//////////logic to determine if nudge is enabled, and so, whether it is active based on he period the user set nudge to be active////////
+// //////////logic to determine if nudge is enabled, and so, whether it is active based on he period the user set nudge to be active////////
 
-var enable_nudge = 1;  // 0 = disable nudge |   1 = enable nudge
-var nudge_start_time;
-var nudge_end_time;
-// get stored nudge start and end time
-function getStoredNudgePeriod (val) {
+// var enable_nudge = 1;  // 0 = disable nudge |   1 = enable nudge
+// var nudge_start_time;
+// var nudge_end_time;
+// // get stored nudge start and end time
+// function getStoredNudgePeriod (val) {
 
-	return new Promise(function(resolve) {
-		chrome.storage.sync.get(['stored_nudge_start_time'], function(data) {
-		          console.log('stored_nudge_start_time is ' + data.stored_nudge_start_time);
-		          nudge_start_time = data.stored_nudge_start_time;
-		});
-		chrome.storage.sync.get(['stored_nudge_end_time'], function(data) {
-		          console.log('stored_nudge_end_time is ' + data.stored_nudge_end_time);
-		          nudge_end_time = data.stored_nudge_end_time;
-		});
-		resolve(val);
-	});
-};
+// 	return new Promise(function(resolve) {
+// 		chrome.storage.sync.get(['stored_nudge_start_time'], function(data) {
+// 		          console.log('stored_nudge_start_time is ' + data.stored_nudge_start_time);
+// 		          nudge_start_time = data.stored_nudge_start_time;
+// 		});
+// 		chrome.storage.sync.get(['stored_nudge_end_time'], function(data) {
+// 		          console.log('stored_nudge_end_time is ' + data.stored_nudge_end_time);
+// 		          nudge_end_time = data.stored_nudge_end_time;
+// 		});
+// 		resolve(val);
+// 	});
+// };
 
-//getStoredNudgePeriod(1).then(function(val){console.log(" val is " + val)});
+// //getStoredNudgePeriod(1).then(function(val){console.log(" val is " + val)});
 
-function checkNudgeOnCriteria (val) {
-	return new Promise(function(resolve) {
+// function checkNudgeOnCriteria (val) {
+// 	return new Promise(function(resolve) {
 
-		chrome.storage.sync.get(['stored_nudge_checkbox'], function(data){	  
-		    if (data.stored_nudge_checkbox == false) {
-		    	//console.log('nudge is disabled because the checkbox check status is ' + data.stored_nudge_checkbox);
-		    	enable_nudge = 0;
-		    }
+// 		chrome.storage.sync.get(['stored_nudge_checkbox'], function(data){	  
+// 		    if (data.stored_nudge_checkbox == false) {
+// 		    	//console.log('nudge is disabled because the checkbox check status is ' + data.stored_nudge_checkbox);
+// 		    	enable_nudge = 0;
+// 		    }
 
-		    else {
+// 		    else {
 
-		    	// console.log('stored_nudge_checkbox is ' + data.stored_nudge_checkbox);
+// 		    	// console.log('stored_nudge_checkbox is ' + data.stored_nudge_checkbox);
 
-		    	var arr1 = nudge_start_time.split(':');
-				var nudge_start_hour = parseInt(arr1[0], 10); 
-				var nudge_start_min = parseInt(arr1[1], 10);
+// 		    	var arr1 = nudge_start_time.split(':');
+// 				var nudge_start_hour = parseInt(arr1[0], 10); 
+// 				var nudge_start_min = parseInt(arr1[1], 10);
 
-				var arr2 = nudge_end_time.split(':');
-				var nudge_end_hour = parseInt(arr2[0], 10);
-				var nudge_end_min = parseInt(arr2[1], 10);
+// 				var arr2 = nudge_end_time.split(':');
+// 				var nudge_end_hour = parseInt(arr2[0], 10);
+// 				var nudge_end_min = parseInt(arr2[1], 10);
 
-				var now = new Date(); // current time
-			    var current_hour = now.getHours();
-			    var current_min = now.getMinutes();
+// 				var now = new Date(); // current time
+// 			    var current_hour = now.getHours();
+// 			    var current_min = now.getMinutes();
 		    	
 
-			    //three scenarios: 
-			    //(a)time is set between 7 pm and 1 am the next morning (start time < end time) 
-			    //(b) between 1 am and 5 am (start time > end time)
-			    //(c) start time < end time
-		    	if (
-		    		(nudge_start_hour < nudge_end_hour && 
-		    		current_hour >= nudge_start_hour && 
-		    		current_hour < nudge_end_hour) 
+// 			    //three scenarios: 
+// 			    //(a)time is set between 7 pm and 1 am the next morning (start time < end time) 
+// 			    //(b) between 1 am and 5 am (start time > end time)
+// 			    //(c) start time < end time
+// 		    	if (
+// 		    		(nudge_start_hour < nudge_end_hour && 
+// 		    		current_hour >= nudge_start_hour && 
+// 		    		current_hour < nudge_end_hour) 
 
-				    ||
+// 				    ||
 
-				    (nudge_start_hour > nudge_end_hour 
-				    &&
-				    ((current_hour >= nudge_start_hour && current_hour >= nudge_end_hour) || (current_hour <= nudge_start_hour && current_hour < nudge_end_hour))
-				    )
+// 				    (nudge_start_hour > nudge_end_hour 
+// 				    &&
+// 				    ((current_hour >= nudge_start_hour && current_hour >= nudge_end_hour) || (current_hour <= nudge_start_hour && current_hour < nudge_end_hour))
+// 				    )
 
-				    ||
+// 				    ||
 
-				    (nudge_start_hour == nudge_end_hour)
-				)
-		    		{
-						enable_nudge = 0;
-						//console.log('nudge is enabled because it is during the period nudge is set to be active');			
-		    		} 
-		    	else {
-		    		enable_nudge = 0;
-		    		//console.log('nudge is disabled because it is NOT during the period nudge is set to be active');
-		    	}
-		    }
-		});
+// 				    (nudge_start_hour == nudge_end_hour)
+// 				)
+// 		    		{
+// 						enable_nudge = 0;
+// 						//console.log('nudge is enabled because it is during the period nudge is set to be active');			
+// 		    		} 
+// 		    	else {
+// 		    		enable_nudge = 0;
+// 		    		//console.log('nudge is disabled because it is NOT during the period nudge is set to be active');
+// 		    	}
+// 		    }
+// 		});
 
-		//console.log("val passed to checkNudgeOnCriteria is: " + val);
-		//console.log("enable nudge value after checkNudgeOnCriteria() is " + enable_nudge);
-		resolve(val);
+// 		//console.log("val passed to checkNudgeOnCriteria is: " + val);
+// 		//console.log("enable nudge value after checkNudgeOnCriteria() is " + enable_nudge);
+// 		resolve(val);
 
-	});
+// 	});
 
-};
-
-
-
-// create an object with list of distracting sites to display nudge 
-let distracting_sites_list = ["facebook", "nytimes", "reddit"];
-chrome.storage.sync.set({stored_distracting_sites_list: distracting_sites_list}, function() {
-	});
-//regular expression to ensure any url and links belonging to a distracting site 
-const facebook = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}facebook\.com(.*)/) ;
-const reddit = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}reddit\.com(.*)/) ;
-const instagram = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}instagram\.com(.*)/) ;
-const twitter = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}twitter\.com(.*)/) ;
-
-const nytimes = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}nytimes\.com(.*)/) ;
-const cnn = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}cnn\.com(.*)/);
-const buzzfeed = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}buzzfeed\.com(.*)/);
-const youtube = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}youtube\.com(.*)/);
-
-const netflix = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}netflix\.com(.*)/);
-const hulu = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}hulu\.com(.*)/);
-const disneyplus = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}hulu\.com(.*)/);
-const hbonow = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}hbonow\.com(.*)/);
-const hbogo = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}hbogo\.com(.*)/);
-
-const tinder = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}tinder\.com(.*)/);
-const pinterest = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}pinterest\.com(.*)/);
-const fandom = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}fandom\.com(.*)/);
-
-var last_nudge_time = new Date('December 17, 1995 22:00:00');
-console.log("last_nudge_time set at installation: " + last_nudge_time);
+// };
 
 
-//console.log(nytimes_match.test('https://jesus.mobile.nytimes.com//world'));
 
-//instagram, cnn, buzzfeed, tumblr, netflix, pinterest, hulu, tinder, imgur, vimeo, amazon, yahoo
-//
+// // create an object with list of distracting sites to display nudge 
+// let distracting_sites_list = ["facebook", "nytimes", "reddit"];
+// chrome.storage.sync.set({stored_distracting_sites_list: distracting_sites_list}, function() {
+// 	});
+// //regular expression to ensure any url and links belonging to a distracting site 
+// const facebook = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}facebook\.com(.*)/) ;
+// const reddit = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}reddit\.com(.*)/) ;
+// const instagram = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}instagram\.com(.*)/) ;
+// const twitter = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}twitter\.com(.*)/) ;
 
-let distracting_site_matches = [facebook, reddit, instagram, twitter, nytimes, cnn, buzzfeed, netflix, hulu, youtube, disneyplus, hbonow, hbogo, tinder, pinterest, fandom];
+// const nytimes = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}nytimes\.com(.*)/) ;
+// const cnn = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}cnn\.com(.*)/);
+// const buzzfeed = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}buzzfeed\.com(.*)/);
+// const youtube = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}youtube\.com(.*)/);
 
-function distracting_site_flag (request) {
-	var result = false;
-	for (var i = 0; i < distracting_site_matches.length; i++) {
-		if (distracting_site_matches[i].test(request) == true) {
-			console.log(i);
-			result = true;
-			console.log("match result is " + result);
-			return (result);
-		}
-	}
-	console.log("match result is " + result);
-	return (result);
-}
+// const netflix = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}netflix\.com(.*)/);
+// const hulu = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}hulu\.com(.*)/);
+// const disneyplus = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}hulu\.com(.*)/);
+// const hbonow = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}hbonow\.com(.*)/);
+// const hbogo = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}hbogo\.com(.*)/);
 
-distracting_site_flag("https://mobile.fb.com///world");
+// const tinder = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}tinder\.com(.*)/);
+// const pinterest = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}pinterest\.com(.*)/);
+// const fandom = new RegExp(/^https?:\/\/([a-zA-Z\d-]+\.){0,}fandom\.com(.*)/);
 
-///logic of nudge redirect
-var nudge_redirect = chrome.extension.getURL('nudge_redirect.html');
-var distracting_site = "https://www.nytimes.com/";
-var tabIdToPreviousUrl = {};
+// var last_nudge_time = new Date('December 17, 1995 22:00:00');
+// console.log("last_nudge_time set at installation: " + last_nudge_time);
 
-//resolveNudge(changeInfo.url, tabIdToPreviousUrl[tabId], tabId, tab.id);
-async function resolveNudge(changeInfo_url, stored_previous_url, tabId, tab_dot_id) {
 
-	let promise = await getStoredNudgePeriod(1).then(function(val) {checkNudgeOnCriteria(val);}).
-                        then(function(val) {});
+// //console.log(nytimes_match.test('https://jesus.mobile.nytimes.com//world'));
 
-    //console.log("DISTRACTING SITE ? " + distracting_site_flag(changeInfo_url));
+// //instagram, cnn, buzzfeed, tumblr, netflix, pinterest, hulu, tinder, imgur, vimeo, amazon, yahoo
+// //
+
+// let distracting_site_matches = [facebook, reddit, instagram, twitter, nytimes, cnn, buzzfeed, netflix, hulu, youtube, disneyplus, hbonow, hbogo, tinder, pinterest, fandom];
+
+// function distracting_site_flag (request) {
+// 	var result = false;
+// 	for (var i = 0; i < distracting_site_matches.length; i++) {
+// 		if (distracting_site_matches[i].test(request) == true) {
+// 			console.log(i);
+// 			result = true;
+// 			console.log("match result is " + result);
+// 			return (result);
+// 		}
+// 	}
+// 	console.log("match result is " + result);
+// 	return (result);
+// }
+
+// distracting_site_flag("https://mobile.fb.com///world");
+
+// ///logic of nudge redirect
+// var nudge_redirect = chrome.extension.getURL('nudge_redirect.html');
+// var distracting_site = "https://www.nytimes.com/";
+// var tabIdToPreviousUrl = {};
+
+// //resolveNudge(changeInfo.url, tabIdToPreviousUrl[tabId], tabId, tab.id);
+// async function resolveNudge(changeInfo_url, stored_previous_url, tabId, tab_dot_id) {
+
+// 	let promise = await getStoredNudgePeriod(1).then(function(val) {checkNudgeOnCriteria(val);}).
+//                         then(function(val) {});
+
+//     //console.log("DISTRACTING SITE ? " + distracting_site_flag(changeInfo_url));
 	
-	//console.log("enable_nudge status after checkNudgeOnCriteria and before the logic  " +  enable_nudge);
+// 	//console.log("enable_nudge status after checkNudgeOnCriteria and before the logic  " +  enable_nudge);
 
-	if (enable_nudge == 0) {
-		console.log("nudge is not active or enabled. distracting sites can be accessed. ");
-		return 0;
-	}
-	else {
-		console.log("nudge is active! ");
-		var previous_url;
+// 	if (enable_nudge == 0) {
+// 		console.log("nudge is not active or enabled. distracting sites can be accessed. ");
+// 		return 0;
+// 	}
+// 	else {
+// 		console.log("nudge is active! ");
+// 		var previous_url;
 		
-		if (changeInfo_url != undefined) {
-			 console.log(" changeInfo_url is: " + changeInfo_url);
-	        //make sure the tab has the right id before getting its url 
-	        if (tabId == tab_dot_id) {
+// 		if (changeInfo_url != undefined) {
+// 			 console.log(" changeInfo_url is: " + changeInfo_url);
+// 	        //make sure the tab has the right id before getting its url 
+// 	        if (tabId == tab_dot_id) {
 	        	
-	        	previousUrl = stored_previous_url;
-	            //console.log("tabId == tab_dot_id");
-	            console.log("stored_previous_url is " + stored_previous_url);
-	        }
-	        else {
-	        	//console.log("tabId !== tab_dot_id");
-	        	//console.log("tabID = " + tabId + " tab_dot_id " + tab_dot_id);
-	        }
-	        if (previousUrl != undefined && previousUrl === nudge_redirect &&  distracting_site_flag(changeInfo_url) == true
-	        	) {
-		          console.log("allows user to continue the site");
-		          chrome.storage.sync.set({stored_last_distracting_site_accessed: changeInfo_url }, function() {});
-		          chrome.tabs.update(tabId, { url: changeInfo_url });
+// 	        	previousUrl = stored_previous_url;
+// 	            //console.log("tabId == tab_dot_id");
+// 	            console.log("stored_previous_url is " + stored_previous_url);
+// 	        }
+// 	        else {
+// 	        	//console.log("tabId !== tab_dot_id");
+// 	        	//console.log("tabID = " + tabId + " tab_dot_id " + tab_dot_id);
+// 	        }
+// 	        if (previousUrl != undefined && previousUrl === nudge_redirect &&  distracting_site_flag(changeInfo_url) == true
+// 	        	) {
+// 		          console.log("allows user to continue the site");
+// 		          chrome.storage.sync.set({stored_last_distracting_site_accessed: changeInfo_url }, function() {});
+// 		          chrome.tabs.update(tabId, { url: changeInfo_url });
 
-	        }
+// 	        }
 
-	        else if (distracting_site_flag(changeInfo_url) == true){
-	        	console.log ("last nudge time is: " + last_nudge_time) ;
-	        	console.log ("current time is: " + Date.now()) ;
-	        	if (Date.now() - last_nudge_time > 1) {
-	        		 chrome.tabs.update(tabId, { url: nudge_redirect });
-	          		console.log("nudge redirects user to breathing practice where they can override it");
-	          		last_nudge_time = Date.now();
-	        	}
+// 	        else if (distracting_site_flag(changeInfo_url) == true){
+// 	        	console.log ("last nudge time is: " + last_nudge_time) ;
+// 	        	console.log ("current time is: " + Date.now()) ;
+// 	        	if (Date.now() - last_nudge_time > 1) {
+// 	        		 chrome.tabs.update(tabId, { url: nudge_redirect });
+// 	          		console.log("nudge redirects user to breathing practice where they can override it");
+// 	          		last_nudge_time = Date.now();
+// 	        	}
 
-    	   		chrome.storage.sync.set({stored_last_distracting_site_accessed: changeInfo_url }, function() {});
+//     	   		chrome.storage.sync.set({stored_last_distracting_site_accessed: changeInfo_url }, function() {});
 	         
-	        }
-	        else {
-	          ;
-	        }
-	        // Add the current url as previous url
-	        tabIdToPreviousUrl[tabId] = changeInfo_url;
-	        //console.log("adding url " + tabIdToPreviousUrl[tabId])
+// 	        }
+// 	        else {
+// 	          ;
+// 	        }
+// 	        // Add the current url as previous url
+// 	        tabIdToPreviousUrl[tabId] = changeInfo_url;
+// 	        //console.log("adding url " + tabIdToPreviousUrl[tabId])
 	       
-	    }
-	    else {
-	    	//console.log(" changeInfo_url is " + changeInfo_url + ".  Ignore this tab update.");
-	    }
+// 	    }
+// 	    else {
+// 	    	//console.log(" changeInfo_url is " + changeInfo_url + ".  Ignore this tab update.");
+// 	    }
 
-	}
-};
+// 	}
+// };
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-		if (changeInfo.url != undefined) {
-			console.log("changeInfo.url == " + changeInfo.url + " | tabIdToPreviousUrl[tabId] == " + tabIdToPreviousUrl[tabId] + "  | tabId ==  " + tabId + " tab.id ==     " +  tab.id);
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+// 		if (changeInfo.url != undefined) {
+// 			console.log("changeInfo.url == " + changeInfo.url + " | tabIdToPreviousUrl[tabId] == " + tabIdToPreviousUrl[tabId] + "  | tabId ==  " + tabId + " tab.id ==     " +  tab.id);
 
-			resolveNudge(changeInfo.url, tabIdToPreviousUrl[tabId], tabId, tab.id);
-		}
-		else {;}
-});
+// 			resolveNudge(changeInfo.url, tabIdToPreviousUrl[tabId], tabId, tab.id);
+// 		}
+// 		else {;}
+// });
 
 
 
---
+++ scheduled_meditation_easing_content_script.js
@@ -0,0 +1,252 @@
+//check if nudge option is on
+//check if it's a time nudge should be on
+//sort through the stored array that contains distracting websites
+//if the current url obtained from wberequest is a match, then run the modal
+
+
+var nudge_start_time;
+var nudge_end_time;
+//var imgURL = chrome.extension.getURL("meditation_flow/morning.jpg");
+
+function injectHTML(val, callback) {
+
+	// $(document).ready(function() {
+		$.get(chrome.extension.getURL('/ease.html'), function(data) {
+	    $(data).appendTo('body');
+	    console.log("ease.html injected");
+		});
+	// });
+
+	if (callback) {
+		callback();
+	}
+	
+};
+
+
+$(document).ready(function() {
+
+chrome.runtime.onMessage.addListener(
+  function(incoming, sender, sendResponse) {
+
+    //console.log("incoming message is " + incoming.message);
+    if (incoming.message == "darken the screen") {
+    	console.log("message to darken the screen is received");
+
+    }
+
+    if (incoming.message == "turn on scheduled meditation") {
+    	exec();
+    	console.log("scheduled meditation is TURNED ON");
+    }
+
+ //    return Promise.resolve("Dummy response to keep the console quiet");
+      
+  }); 
+
+
+});
+
+
+
+function displayNudge () {
+	$(document).ready(function() {
+		var time = 1000;
+		var interval;
+
+		function generateTime(timer_length) {
+			time = time - 1000;     
+			// var minutes = Math.floor((time % (1000 * 60 * 60))/(1000 * 60)); 
+			// var seconds = Math.floor((time % (1000 * 60)) / 1000);
+			// document.getElementById("timer_control535676465768").innerHTML = "0" + minutes + ":" + seconds ;
+		}
+		//update the count down every 1 second
+		var x = function updateTimer() {     
+			generateTime(time);
+			if (time < 0) {         
+				clearInterval(interval);
+				// var bell_sound = document.getElementById("zenbellsound");
+				// bell_sound.play();
+
+				//bring user back to their work website
+				var quit_site_button = document.getElementById("timer_control535676465768")
+				quit_site_button.innerHTML = "return to work";
+				quit_site_button.style.cursor= "pointer";
+				quit_site_button.addEventListener("click", function() {
+						window.location = "https://www.google.com";
+					}
+				);
+
+			var modal = document.getElementById("myModal472826662848262673");
+			var close_button_174162884722728293 = document.getElementById("close_button_174162884722728293");
+
+			close_button_174162884722728293.addEventListener("click", function() {
+					modal.style.display = "none";
+				}
+			);
+
+			// $('#user_profile_icon_9809403065').on('click', function(event) {
+				
+			// 	$('#myModal472826662848262673').hide();
+			// });
+
+			//close the modal
+			// close_button_174162884722728293.onclick = function() {
+			//  modal.style.display = "none";
+			// };
+			//When the user clicks anywhere outside of the modal, close it
+			window.onclick = function(event) {
+			  if (event.target == modal) {
+			    modal.style.display = "none";  
+			  }
+			};
+
+			}	
+		};
+		function startTimer() {
+			//var bell_sound = document.getElementById("zenbellsound");
+			// bell_sound.play();
+			// document.getElementById("timer_control").style.cursor= "initial";
+			interval = setInterval(x, 1000);
+		}
+		startTimer();
+
+		// 
+		// Get the close element that closes the modal
+
+		// function dummy () {
+
+		// 	var modal = document.getElementById("myModal472826662848262673");
+		// 	var close_button_174162884722728293 = document.getElementById("close_button_174162884722728293");
+
+		// 	close_button_174162884722728293.addEventListener("click", function() {
+		// 			modal.style.display = "none";
+		// 		}
+		// 	);
+
+		// 	$('#user_profile_icon_9809403065').on('click', function(event) {
+				
+		// 		$('#myModal472826662848262673').hide();
+		// 	});
+
+		// 	//close the modal
+		// 	close_button_174162884722728293.onclick = function() {
+		// 	 modal.style.display = "none";
+		// 	};
+		// 	//When the user clicks anywhere outside of the modal, close it
+		// 	window.onclick = function(event) {
+		// 	  if (event.target == modal) {
+		// 	    modal.style.display = "none";  
+		// 	  }
+		// 	};
+
+
+		// };
+
+
+
+		}); //ready
+};
+
+ 
+//////////logic to decide whether the modal should be displayed or not////////
+
+// get stored nudge start and end time
+function getStoredNudgePeriod (val) {
+
+	return new Promise(function(resolve) {
+		chrome.storage.sync.get(['stored_nudge_start_time'], function(data) {
+		          console.log('stored_nudge_start_time is ' + data.stored_nudge_start_time);
+		          nudge_start_time = data.stored_nudge_start_time;
+		});
+		chrome.storage.sync.get(['stored_nudge_end_time'], function(data) {
+		          console.log('stored_nudge_end_time is ' + data.stored_nudge_end_time);
+		          nudge_end_time = data.stored_nudge_end_time;
+		});
+		resolve(val);
+	});
+};
+
+//getStoredNudgePeriod(1).then(function(val){console.log(" val is " + val)});
+
+function checkNudgeOnCriteria (val) {
+
+
+	return new Promise(function(resolve) {
+
+		
+
+		chrome.storage.sync.get(['stored_nudge_checkbox'], function(data){	    
+		    if (data.stored_nudge_checkbox == false) {
+		    	// console.log('stored_nudge_checkbox is ' + data.stored_nudge_checkbox + ' so no nudge');
+		    }
+
+		    else {
+
+		    	// console.log('stored_nudge_checkbox is ' + data.stored_nudge_checkbox);
+
+		    	var arr1 = nudge_start_time.split(':');
+				var nudge_start_hour = parseInt(arr1[0], 10); 
+				var nudge_start_min = parseInt(arr1[1], 10);
+
+				var arr2 = nudge_end_time.split(':');
+				var nudge_end_hour = parseInt(arr2[0], 10);
+				var nudge_end_min = parseInt(arr2[1], 10);
+
+				var now = new Date(); // current time
+			    var current_hour = now.getHours();
+			    var current_min = now.getMinutes();
+		    	
+
+			    //three scenarios: 
+			    //(a)time is set between 7 pm and 1 am the next morning (start time < end time) 
+			    //(b) between 1 am and 5 am (start time > end time)
+			    //(c) start time < end time
+		    	if (
+		    		(nudge_start_hour < nudge_end_hour && 
+		    		current_hour >= nudge_start_hour && 
+		    		current_hour < nudge_end_hour) 
+
+				    ||
+
+				    (nudge_start_hour > nudge_end_hour 
+				    &&
+				    ((current_hour >= nudge_start_hour && current_hour >= nudge_end_hour) || (current_hour <= nudge_start_hour && current_hour < nudge_end_hour))
+				    )
+
+				    ||
+
+				    (nudge_start_hour == nudge_end_hour)
+				)
+
+		    		{
+						injectHTML(0,function() {
+							displayNudge();
+						});
+						
+		    		} 
+		    }
+		});
+
+		//console.log("val passed to checkNudgeOnCriteria is: " + val);
+
+		resolve(val);
+
+	});
+
+};
+
+getStoredNudgePeriod(1).then(function(val) {checkNudgeOnCriteria(val);}).
+                        then(function(val) {console.log(" value passed  to last then is " +  val)});
+
+
+// getStoredNudgePeriod(0, function() {
+// 	checkNudgeOnCriteria(0);
+// });
+
+
+
+
+
+
+
